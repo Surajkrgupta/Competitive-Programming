ac3e971f60343bb90ee8f4d999fcb29be0b1f3dc
Suraj Kumar Gupta <124661478+Surajkrgupta@users.noreply.github.com>
2026-05-21 05:50:15 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+       public static void main(String args[]){
+              Scanner sc=new Scanner(System.in);
+              int t=sc.nextInt();
+              while(t--!=0){
+              int a=sc.nextInt();
+              int b=sc.nextInt();
+              int c=sc.nextInt();
+              if(c%2==0){
+                     if(a>b) System.out.println("First");
+                     else System.out.println("Second");
+              }
+              else {
+                     if(b>a)System.out.println("Second");
+                     else System.out.println("First");
+              }
+              }
+       }
+}
